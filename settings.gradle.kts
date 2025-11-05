5d4b54b94721526286155bbe9259be9c1170f971
eiowlnxd-cyber <eiowlnxd@gmail.com>
2025-11-06 04:08:02 +0800

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+pluginManagement {
+    repositories {
+        maven("https://maven.aliyun.com/repository/central")
+        maven("https://maven.aliyun.com/repository/google")
+        maven("https://maven.aliyun.com/repository/gradle-plugin")
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        maven("https://maven.aliyun.com/repository/central")
+        maven("https://maven.aliyun.com/repository/google")
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MyTimoApp"
+include(":app")
