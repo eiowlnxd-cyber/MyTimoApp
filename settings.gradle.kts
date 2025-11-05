a6c04dee80e79f168ba04b293f3613fdc2243f16
eiowlnxd-cyber <eiowlnxd@gmail.com>
2025-11-06 07:28:15 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,7 @@
 pluginManagement {
     repositories {
-        // 添加阿里云和华为云镜像作为备用仓库
         maven { url = uri("https://maven.aliyun.com/repository/public") }
         maven { url = uri("https://repo.huaweicloud.com/repository/maven/") }
-        
-        // 保留官方仓库作为最终选择
         google()
         mavenCentral()
         gradlePluginPortal()
@@ -13,11 +10,8 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        // 同样为依赖项添加备用仓库
         maven { url = uri("https://maven.aliyun.com/repository/public") }
         maven { url = uri("https://repo.huaweicloud.com/repository/maven/") }
-        
-        // 保留官方仓库
         google()
         mavenCentral()
     }
