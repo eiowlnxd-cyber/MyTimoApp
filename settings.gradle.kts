954a417d45cb3758b1eb0e66707ba3adb831ba9b
eiowlnxd-cyber <eiowlnxd@gmail.com>
2025-11-06 06:03:23 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,22 +1,16 @@
 pluginManagement {
     repositories {
-        maven("https://maven.aliyun.com/repository/central")
-        maven("https://maven.aliyun.com/repository/google")
-        maven("https://maven.aliyun.com/repository/gradle-plugin")
-        gradlePluginPortal()
         google()
         mavenCentral()
+        gradlePluginPortal()
     }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven("https://maven.aliyun.com/repository/central")
-        maven("https://maven.aliyun.com/repository/google")
         google()
         mavenCentral()
     }
 }
-
 rootProject.name = "MyTimoApp"
 include(":app")
